a7651d5d1789a110ccc06c27a1bc266f76eadcab
Adilson Ramos <junior.ramos.contato@gmail.com>
2025-04-03 17:29:39 -0300

    Text Preset - Conectando ao Design System

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+
 import { Text } from './src/components/Text/Text';
 
 function App(): React.JSX.Element {
 
   return (
     <SafeAreaView >
-      <Text>
+      <Text preset="headingLarge" style={{color: 'red'}}>
+        Coffstack
+      </Text>
+      <Text preset="headingMedium" style={{color: 'blue'}}>
+        Coffstack
+      </Text>
+      <Text preset="headingSmall">
         Coffstack
       </Text>
     </SafeAreaView>
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { Text as RNText, TextProps, TextStyle } from 'react-native'
+import { Text as RNText, TextProps as RNTextProps, TextStyle } from 'react-native'
 
-export function Text({children, ...rest}: TextProps) {
+interface TextProps extends RNTextProps {
+    preset?: TextVariants;
+}
+export function Text({children, preset = 'paragraphMedium', style, ...rest}: TextProps) {
     return (
-        <RNText style={fontSizes.paragraphLarge} {...rest}>
+        <RNText style={[$fontSizes[preset], style]} {...rest}>
             {children}
         </RNText>
     );
@@ -11,7 +14,7 @@ export function Text({children, ...rest}: TextProps) {
 
 type TextVariants = 'headingLarge' | 'headingMedium' | 'headingSmall' | 'paragraphLarge' | 'paragraphMedium' | 'paragraphSmall' | 'paragraphCaption' | 'paragraphCaptionSmall';
 
-const fontSizes: Record<TextVariants, TextStyle> = {
+const $fontSizes: Record<TextVariants, TextStyle> = {
     headingLarge: {fontSize: 32, lineHeight: 38.4},
     headingMedium: {fontSize: 22, lineHeight: 26.4},
     headingSmall: {fontSize: 18, lineHeight: 23.4},
