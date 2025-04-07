a66d5eaf5e195fe4bd9d69cefdb8c5d28b3993cd
Adilson Ramos <junior.ramos.contato@gmail.com>
2025-04-07 19:59:37 -0300

    Button

--- App.tsx
+++ App.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 
 import { Text } from './src/components/Text/Text';
+import { Button } from './src/components/Button/Button';
 
 function App(): React.JSX.Element {
 
   return (
     <SafeAreaView >
-      <Text italic bold preset="headingLarge">
-        Coffstack
-      </Text>
-      <Text preset="headingLarge">
-        Coffstack
-      </Text>
-      <Text preset="headingSmall">
-        Coffstack
-      </Text>
+      <View style={{paddingHorizontal: 24}} >
+        <Text italic bold preset="headingLarge">
+          Coffstack
+        </Text>
+        <Button title="Entrar" />
+      </View>
     </SafeAreaView>
   );
 }
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {TouchableOpacity} from 'react-native';
+import {Text} from '../Text/Text';
+
+interface ButtonProps {
+  title: string;
+}
+
+export function Button({title}: ButtonProps) {
+  return (
+    <TouchableOpacity
+      style={{
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        backgroundColor: '#074C4E',
+        alignItems: 'center',
+        borderRadius: 16
+      }}>
+      <Text preset='paragraphMedium' bold style={{color: 'white'}}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
