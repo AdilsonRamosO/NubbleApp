f4624af63f19636a8611e9b1ecbf30a4bf642cb7
Adilson Ramos <junior.ramos.contato@gmail.com>
2025-04-08 20:46:16 -0300

    Restyle - Criando o tema no App

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
+import {ThemeProvider} from '@shopify/restyle';
 import React from 'react';
 import {SafeAreaView, View} from 'react-native';
+import {Button} from './src/components/Button/Button';
 
-import { Text } from './src/components/Text/Text';
-import { Button } from './src/components/Button/Button';
+import {Text} from './src/components/Text/Text';
+import {theme} from './src/theme/theme';
 
 function App(): React.JSX.Element {
 
   return (
-    <SafeAreaView >
-      <View style={{paddingHorizontal: 24}} >
-        <Text italic bold preset="headingLarge">
-          Coffstack
-        </Text>
-        <Button title="Entrar" />
-      </View>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <View style={{paddingHorizontal: 24}}>
+          <Text italic bold preset="headingLarge">
+            Coffstack
+          </Text>
+          <Button title="Entrar" />
+        </View>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
