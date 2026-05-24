1d1a5ea72955de463eabbcc7d85065e9bf9c792f
AdilsonRamosO <junior.ramos.contato@gmail.com>
2026-05-24 15:48:42 -0300

    Conectando ao Design System

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
-import  {SafeAreaView} from 'react-native';
-import { Text } from './src/components/Text/Text';
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+
+import {Text} from './src/components/Text/Text';
 
 function App() {
   return (
     <SafeAreaView>
-      <Text>Hello World</Text>
+      <Text preset="headingLarge" style={{color: 'red'}}>
+        Hello World
+      </Text>
+      <Text preset="paragraphCaption" style={{color: 'red'}}>
+        Hello World
+      </Text>
+      <Text preset="paragraphMedium">Hello World</Text>
     </SafeAreaView>
-  )
+  );
 }
 
 export default App;
